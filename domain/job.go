edc0416ddc75940472524bf7be9f16db680355c2
poring86 <matheuslino86a@gmail.com>
2024-03-07 19:43:08 -0400

    feat: :sparkles: Entities

--- domain/job.go
+++ domain/job.go
@@ -12,20 +12,22 @@ func init() {
 }
 
 type Job struct {
-	ID               string    `valid:"uuid"`
-	OutputBucketPath string    `valid:"notnull"`
-	Status           string    `valid:"notnull"`
-	Video            *Video    `valid:"-"`
-	VideoID          string    `valid:"-"`
+	ID               string    `json:"job_id" valid:"uuid" gorm:"type:uuid;primary_key"`
+	OutputBucketPath string    `json:"output_bucket_path" valid:"notnull"`
+	Status           string    `json:"status" valid:"notnull"`
+	Video            *Video    `json:"video" valid:"-"`
+	VideoID          string    `json:"-" valid:"-" gorm:"column:video_id;type:uuid;notnull"`
 	Error            string    `valid:"-"`
-	CreatedAt        time.Time `valid:"-"`
-	UpdatedAt        time.Time `valid:"-"`
+	CreatedAt        time.Time `json:"created_at" valid:"-"`
+	UpdatedAt        time.Time `json:"updated_at" valid:"-"`
 }
 
 func NewJob(output string, status string, video *Video) (*Job, error) {
+
 	job := Job{
 		OutputBucketPath: output,
 		Status:           status,
+		Video:            video,
 	}
 
 	job.prepare()
@@ -37,6 +39,15 @@ func NewJob(output string, status string, video *Video) (*Job, error) {
 	}
 
 	return &job, nil
+
+}
+
+func (job *Job) prepare() {
+
+	job.ID = uuid.NewV4().String()
+	job.CreatedAt = time.Now()
+	job.UpdatedAt = time.Now()
+
 }
 
 func (job *Job) Validate() error {
@@ -47,10 +58,4 @@ func (job *Job) Validate() error {
 	}
 
 	return nil
-}
-
-func (job *Job) prepare() {
-	job.ID = uuid.NewV4().String()
-	job.CreatedAt = time.Now()
-	job.UpdatedAt = time.Now()
 }
